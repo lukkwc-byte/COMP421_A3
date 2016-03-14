7be22d863c393b71156da4c58c66b39bdd32a736
nikelaren <luk.kwc@gmail.com>
2016-03-13 22:04:15 -0400

    updates to the interface

--- interface.java
+++ interface.java
@@ -2,27 +2,27 @@ public void Greeting() {
 	System.out.println();
 	System.out.println("****************************************************************");
 	System.out.println();
+	System.out.println("		McGill Health Network Patient Management System");
 	System.out.println("");
-	System.out.println("");
-	System.out.println("This system will allow you to access and modify the health records of the hospital");
+	System.out.println("By Group 52: Kevin Luk, Fan Ma, Jordan Quan, Gabriela Stefanini");
 	System.out.println();
 	System.out.println("****************************************************************");
 	System.out.println();
 	Scanner scan = new Scanner(System.in);
-	boolean exit = false;
+}
+public void Menu() {
+	System.out.println("Please enter the number corresponding to the option desired.");
+	System.out.println("	(1) Admit a new patient"
+					+ " 	(2) Add new staff \n"
+					+ "		(3) See all hospitals patient capacity statuses\n"
+					+ "		(4) Print all patients records\n"
+					+ "		(5) Find avg. length of treatment plan at each hospital for a certain disease\n"
+					+ "		(6) Reassign a nurse to a new team\n"
+					+ "		(7) Exit program"
+	System.out.print("Enter selection here: ");
+}
 
-public void Menu () {
-	while (!exit) {
+public void DisplayOption1(){
 
-			System.out
-					.println("Please select one of the following options and click enter:");
-			System.out.println("   (1) Add a new patient to the EMR system\n"
-					+ "   (2) Add a new Doctor to the EMR system\n"
-					+ "   (3) Record new patient visit to the department\n"
-					+ "   (4) Edit patient information\n"
-					+ "   (5) Display list of all Patient IDs\n"
-					+ "   (6) Display list of all Doctor IDs\n"
-					+ "   (7) Print a Patient's record\n"
-					+ "   (8) Print a Doctor's record\n"
-					+ "   (9) Exit and save modifications\n");
-			System.out.print("   ENTER YOUR SELECTION HERE: ");}
+}
+
